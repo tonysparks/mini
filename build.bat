4793cf9fa5ef5d0fd646097fb990ee0b6e0b9dd0
tonysparks <anthony.sparks31@gmail.com>
2019-06-15 22:48:35 -0500

    remove dead code

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
 @echo off
-REM cd C:\Users\chq-tonys\Desktop\scripts\
 set LITAC_PATH=C:\Users\antho\eclipse-workspace\litac\lib
 litac.exe -run -lib "%LITAC_PATH%" -buildCmd "clang.exe -o %%output%% %%input%% -D_CRT_SECURE_NO_WARNINGS -I../include -L../lib -lraylib.lib" -outputDir "./bin" "./src/main.lita"
