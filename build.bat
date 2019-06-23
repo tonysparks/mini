b36d9dc4cba8cf4fb04f128a5b365fb97f75391a
tonysparks <anthony.sparks31@gmail.com>
2019-06-22 22:32:52 -0500

    added wren and console

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 set LITAC_PATH=C:\Users\antho\eclipse-workspace\litac\lib
-litac.exe -run -lib "%LITAC_PATH%" -buildCmd "clang.exe -o %%output%% %%input%% -D_CRT_SECURE_NO_WARNINGS -I../include -L../lib -lraylib.lib" -outputDir "./bin" "./src/main.lita"
+litac.exe -run -lib "%LITAC_PATH%" -buildCmd "clang.exe -o %%output%% %%input%% -D_CRT_SECURE_NO_WARNINGS -I../include -L../lib -lraylib.lib -lwren_static_d.lib" -outputDir "./bin" "./src/main.lita"
