b8fa4017ed8fe414b37b098cca8ce354e23d9e7d
tonysparks <anthony.sparks31@gmail.com>
2019-07-20 17:31:34 -0500

    able to read building information from map.json

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 @echo off
 set LITAC_PATH=C:\Users\antho\eclipse-workspace\litac\lib
+del ".\bin\mini.*" /q
 litac.exe -run -lib "%LITAC_PATH%" -buildCmd "clang.exe -g -fsanitize=undefined,address -o %%output%% %%input%% -D_CRT_SECURE_NO_WARNINGS -I../include -L../lib -lraylib.lib -lwren_static.lib" -outputDir "./bin" -output "mini" "./src/main.lita" -types
